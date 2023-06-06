c7fa87cc3213d64a2c5a01c69e8db378fcd0f750
rad234 <69139818+rad234@users.noreply.github.com>
2023-06-06 15:30:36 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function marcheazaUnu() 
-{
-document.getElementById("status-unu").innerText="Articol citit în sesiunea aceasta"
-}
-
-function marcheazaDoi() 
-{
-document.getElementById("status-doi").innerText="Articol citit în sesiunea aceasta"
-}
